58e5e69cac4cbde54907c69b43547dafd60bf845
Denys Kovika <d.v.kovika@gmail.com>
2022-07-08 17:37:58 +0300

    ver.1

--- src/main/java/com/epam/rd/autotasks/meetautocode/ElectronicWatch.java
+++ src/main/java/com/epam/rd/autotasks/meetautocode/ElectronicWatch.java
@@ -6,7 +6,12 @@ public class ElectronicWatch {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int seconds = scanner.nextInt();
+        int chch;
 
+        seconds %= 86400;
+        chch = seconds/3600;
+
+        System.out.printf("%01d:%02d:%02d", chch, (seconds-chch*3600)/60, seconds%60);
 
     }
 }
